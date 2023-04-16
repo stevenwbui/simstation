8d94ca6b85abf7323c6cc870145e6eee5a68d048
ashnapat <52472081+ashnapat@users.noreply.github.com>
2023-04-16 12:45:23 -0700

    Delete FlockingFactory.java

--- src/FlockingFactory.java
+++
@@ -1,9 +0,0 @@
-package flocking;
-
-import mvc.Model;
-import simstation.SimStationFactory;
-
-public class FlockingFactory extends SimStationFactory {
-    public Model makeModel() { return new FlockingSimulation(); }
-    public String getTitle() { return "Flocking Simulator";}
-}
