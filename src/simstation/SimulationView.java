511ccbe7f7508eae488a6c3501391619893359e9
mic0430 <chulaphang.25@gmail.com>
2023-04-15 21:14:14 -0700

    view

--- src/simstation/SimulationView.java
+++ src/simstation/SimulationView.java
@@ -3,8 +3,28 @@ package simstation;
 import mvc.Model;
 import mvc.View;
 
+import java.awt.*;
+import java.awt.geom.Ellipse2D;
+import java.util.Iterator;
+
 public class SimulationView extends View {
-    public SimulationView(Model model) {
-        super(model);
+    final int AGENT_DIAMETER = 5;
+    public SimulationView(Simulation sim) {
+        super(sim);
+    }
+
+    public void paintComponent(Graphics gc) {
+        super.paintComponent(gc);
+        Color oldColor = gc.getColor();
+        Simulation sim = (Simulation) model;
+        Iterator<Agent> it = sim.iterator();
+        while (it.hasNext()) {
+            Agent a =it.next();
+            gc.setColor(a.getColor());
+            Graphics2D g2d = (Graphics2D) gc;
+            Ellipse2D.Double point = new Ellipse2D.Double(a.getXc(), a.getYc(), AGENT_DIAMETER, AGENT_DIAMETER);
+            g2d.fill(point);
+        }
+        gc.setColor(oldColor);
     }
 }
